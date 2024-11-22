bc52fb991eea110fe9cad8d38f5f6dd4491fa324
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-22 19:30:39 +0100

    feat: added organisation email

--- plugin-build/buildSrc/src/main/kotlin/Project.kt
+++ plugin-build/buildSrc/src/main/kotlin/Project.kt
@@ -1,7 +1,8 @@
 object Project {
     const val NAME = "Kreate"
     const val DESCRIPTION = "A Gradle plugin to simplify the creation of Davils projects."
-    const val ORGANIZATION_NAME = "Davils"
-    const val ORGANISATION_URL = "https://davils.net"
     const val GROUP = "net.davils.kreate"
+    const val ORGANIZATION_NAME = "Davils"
+    const val ORGANIZATION_EMAIL = "contact@davils.net"
+    const val ORGANISATION_URL = "https://davils.net"
 }
