542ee9000f658b89388bbc0535cd844cebe34093
Nils Jäkel <nils.jaekel@davils.net>
2024-11-22 18:33:54 +0100

    refactor: changed extension func to the abstract class

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/KreateExtension.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/KreateExtension.kt
@@ -1,17 +1,24 @@
 package net.davils.kreate
 
 import net.davils.kreate.core.CoreConfiguration
+import net.davils.kreate.publishing.PublishingConfiguration
 import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.tasks.Nested
 
-public interface KreateExtension {
+public abstract class KreateExtension {
     @get:Nested
-    public val core: CoreConfiguration
-}
+    public abstract val core: CoreConfiguration
+    @get:Nested
+    public abstract val publishing: PublishingConfiguration
 
-public fun KreateExtension.core(action: Action<CoreConfiguration>) {
-    action.execute(core)
+    public fun core(action: Action<CoreConfiguration>) {
+        action.execute(core)
+    }
+
+    public fun publishing(action: Action<PublishingConfiguration>) {
+        action.execute(publishing)
+    }
 }
 
 public interface KreateFeature {
