b98150420ba152c646cfa7c5c3f7a39ce6c83c37
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-22 20:59:34 +0100

    feat: added task to patch the version in not gradle projects

---
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/core/PatchVersions.kt
@@ -0,0 +1,24 @@
+package net.davils.kreate.core
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+public abstract class PatchVersions : DefaultTask() {
+    private val version = System.getenv("CI_COMMIT_TAG") ?: System.getenv("CI_COMMIT_SHORT_SHA")?.let { "$it-dev" } ?: "0.0.0"
+
+    @get:Input
+    public abstract val files: ListProperty<File>
+
+    @TaskAction
+    internal fun patch() {
+        val files = files.get()
+        files.forEach { file ->
+            val content = file.readText()
+            val regex = Regex("\\d+\\.\\d+\\.\\d+")
+            file.writeText(content.replace(regex, version))
+        }
+    }
+}
