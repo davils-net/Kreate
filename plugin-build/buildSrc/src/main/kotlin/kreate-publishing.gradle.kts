853a8897092eee451d273c9c67c41edf77e54971
Nils Jäkel <nils.jaekel@davils.net>
2024-11-22 10:23:53 +0100

    feat: added publishing logic

---
+++ plugin-build/buildSrc/src/main/kotlin/kreate-publishing.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            pom {
+                name = Project.NAME
+                description = Project.DESCRIPTION
+
+                developers {
+                    developer {
+                        id = "davils"
+                        name = "Davils"
+                        email = "contact@davils.net"
+                        organization = "Davils"
+                        organizationUrl = Project.ORGANISATION_URL
+                        url = Project.ORGANISATION_URL
+                        timezone = "Europe/Berlin"
+                    }
+                }
+
+                licenses {
+                    license {
+                        name = "All rights reserved"
+                    }
+                }
+
+                issueManagement {
+                    system = "YouTrack"
+                }
+            }
+
+            repositories {
+                if (System.getenv(/* name = */ "CI_JOB_TOKEN") != null) {
+                    maven {
+                        name = "GitLab"
+
+                        val projectId = System.getenv(/* name = */ "CI_PROJECT_ID")
+                        val apiV4 = System.getenv(/* name = */ "CI_API_V4_URL")
+                        url = uri("$apiV4/projects/$projectId/packages/maven")
+
+                        authentication {
+                            create(/* name = */ "token", /* type = */ HttpHeaderAuthentication::class.java) {
+                                credentials(HttpHeaderCredentials::class.java) {
+                                    name = "Job-Token"
+                                    value = System.getenv(/* name = */ "CI_JOB_TOKEN")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
