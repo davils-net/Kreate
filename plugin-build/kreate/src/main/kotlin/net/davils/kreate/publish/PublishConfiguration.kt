ed0b500eadde2e01dafa78771c197592285b405a
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-22 19:27:19 +0100

    refactor: changed root package name

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/publish/PublishConfiguration.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/publish/PublishConfiguration.kt
@@ -1,8 +1,8 @@
-package net.davils.kreate.publishing
+package net.davils.kreate.publish
 
 import org.gradle.api.provider.Property
 
-public interface PublishingConfiguration {
+public interface PublishConfiguration {
     public val enabled: Property<Boolean>
     public val inceptionYear: Property<Int>
 }
