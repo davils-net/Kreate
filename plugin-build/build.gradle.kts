18b917b5cf0583d01f8bf88469d25ac772dca9a0
Nils Jäkel <nils.jaekel@davils.net>
2024-11-22 10:24:11 +0100

    feat: added kreate publishing

--- plugin-build/build.gradle.kts
+++ plugin-build/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    `kreate-publishing`
+}
+
 allprojects {
     version = System.getenv("CI_COMMIT_TAG") ?: System.getenv("CI_COMMIT_SHORT_SHA")?.let { "$it-dev" } ?: "0.0.0"
     group = Project.GROUP
