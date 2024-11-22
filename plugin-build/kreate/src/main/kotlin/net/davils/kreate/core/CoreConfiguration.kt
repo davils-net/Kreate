1851bfdc466895e377c56441df0b3c9d92998855
Nils Jäkel <nils.jaekel@davils.net>
2024-11-22 18:34:35 +0100

    feat: added func for patching version

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/core/CoreConfiguration.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/core/CoreConfiguration.kt
@@ -1,9 +1,20 @@
 package net.davils.kreate.core
 
 import org.gradle.api.provider.Property
+import java.io.File
 
 public interface CoreConfiguration {
     public val name: Property<String>
     public val description: Property<String>
     public val license: Property<License>
+
+    public fun patchVersionsInFiles(vararg files: File) {
+        files.forEach { file ->
+            patchVersionsInFile(file)
+        }
+    }
+
+    public fun patchVersionsInFile(file: File) {
+
+    }
 }
