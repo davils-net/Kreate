c434087452dc2cae2f088aad6e5b1925e799226b
Nils Jäkel <nils.jaekel@davils.net>
2024-11-22 10:24:41 +0100

    feat: added name, description and organisation url as properties

--- plugin-build/buildSrc/src/main/kotlin/Project.kt
+++ plugin-build/buildSrc/src/main/kotlin/Project.kt
@@ -1,3 +1,6 @@
 object Project {
+    const val NAME = "Kreate"
+    const val DESCRIPTION = "A Gradle plugin to simplify the creation of Davils projects."
+    const val ORGANISATION_URL = "https://davils.net"
     const val GROUP = "net.davils"
 }
