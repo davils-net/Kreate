984a169494b130fd9a5976c0193e97e4591725b7
Nils Jäkel <nils.jaekel@davils.net>
2024-11-22 09:47:02 +0100

    feat: added plugin build module

---
+++ plugin-build/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "net.davils"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,5 @@ plugins {
 }
 rootProject.name = "Kreate"
 
+include("example")
+includeBuild("plugin-build")
