38c3c319a9bad0827d27d257f2cb93bd31a92ba6
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-23 21:53:25 +0100

    feat: use default configuration

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/KreateExtension.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/KreateExtension.kt
@@ -1,35 +1,35 @@
 package net.davils.kreate
 
-import net.davils.kreate.feature.buildconstants.BuildConstantsConfiguration
-import net.davils.kreate.feature.core.CoreConfiguration
-import net.davils.kreate.feature.cinterop.CinteropConfiguration
-import net.davils.kreate.feature.publish.PublishConfiguration
+import net.davils.kreate.feature.buildconstants.DefaultBuildConstantsConfiguration
+import net.davils.kreate.feature.cinterop.DefaultCinteropConfiguration
+import net.davils.kreate.feature.core.DefaultCoreConfiguration
+import net.davils.kreate.feature.publish.DefaultPublishConfiguration
 import org.gradle.api.Action
 import org.gradle.api.tasks.Nested
 
 public abstract class KreateExtension {
     @get:Nested
-    public abstract val core: CoreConfiguration
+    public abstract val core: DefaultCoreConfiguration
     @get:Nested
-    public abstract val publishing: PublishConfiguration
+    public abstract val publish: DefaultPublishConfiguration
     @get:Nested
-    public abstract val buildConstants: BuildConstantsConfiguration
+    public abstract val buildConstants: DefaultBuildConstantsConfiguration
     @get:Nested
-    public abstract val cinterop: CinteropConfiguration
+    public abstract val cinterop: DefaultCinteropConfiguration
 
-    public fun core(action: Action<CoreConfiguration>) {
+    public fun core(action: Action<DefaultCoreConfiguration>) {
         action.execute(core)
     }
 
-    public fun publishing(action: Action<PublishConfiguration>) {
-        action.execute(publishing)
+    public fun publish(action: Action<DefaultPublishConfiguration>) {
+        action.execute(publish)
     }
 
-    public fun buildConstants(action: Action<BuildConstantsConfiguration>) {
+    public fun buildConstants(action: Action<DefaultBuildConstantsConfiguration>) {
         action.execute(buildConstants)
     }
 
-    public fun cinterop(action: Action<CinteropConfiguration>) {
+    public fun cinterop(action: Action<DefaultCinteropConfiguration>) {
         action.execute(cinterop)
     }
 }
