79b0245c244716d6967534d6f07d886e1c43a786
Nils Jäkel <nils.jaekel@redtronics.dev>
2024-11-23 14:59:58 +0100

    feat: used new registration feature for sub plugins

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/Kreate.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/Kreate.kt
@@ -1,8 +1,9 @@
 package net.davils.kreate
 
-import net.davils.kreate.buildconstants.BuildConstants
-import net.davils.kreate.core.Core
-import net.davils.kreate.publish.Publish
+import net.davils.kreate.feature.buildconstants.BuildConstants
+import net.davils.kreate.feature.core.Core
+import net.davils.kreate.feature.cinterop.Cinterop
+import net.davils.kreate.feature.publish.Publish
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.create
@@ -11,8 +12,9 @@ public class Kreate : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create("kreate", KreateExtension::class)
 
-        Core.apply(project, extension)
-        Publish.apply(project, extension)
-        BuildConstants.apply(project, extension)
+        Core(project, extension).apply()
+        Cinterop(project, extension).apply()
+        BuildConstants(project, extension).apply()
+        Publish(project, extension).apply()
     }
 }
