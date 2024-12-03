6523c757e77a0a7940b96eff17f2f784f0bfcd47
Nils Jäkel <nils.jaekel@davils.net>
2024-12-03 11:12:38 +0100

    docs: added docstring for kreate

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/Kreate.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/Kreate.kt
@@ -2,7 +2,7 @@ package net.davils.kreate
 
 import net.davils.kreate.feature.buildconstants.BuildConstants
 import net.davils.kreate.feature.core.Core
-import net.davils.kreate.feature.cinterop.Cinterop
+import net.davils.kreate.feature.cinterop.CInterop
 import net.davils.kreate.feature.docs.Docs
 import net.davils.kreate.feature.publish.Publish
 import net.davils.kreate.feature.testing.Testing
@@ -10,12 +10,18 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.create
 
+/**
+ * Entry point for the Kreate plugin.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
 public class Kreate : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create("kreate", KreateExtension::class)
 
         Core(project, extension).apply()
-        Cinterop(project, extension).apply()
+        CInterop(project, extension).apply()
         BuildConstants(project, extension).apply()
         Testing(project, extension).apply()
         Docs(project, extension).apply()
