4de9c62ef52c0b339aa94ac08092a86377a1cd7e
Nils Jäkel <nils.jaekel@davils.net>
2024-12-03 11:12:29 +0100

    docs: added docstring for kreate extension

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/KreateExtension.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/KreateExtension.kt
@@ -9,40 +9,135 @@ import net.davils.kreate.feature.testing.DefaultTestingConfiguration
 import org.gradle.api.Action
 import org.gradle.api.tasks.Nested
 
+/**
+ * Kreate extension to configure the entire plugin.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
 public abstract class KreateExtension {
+    /**
+     * The core configuration of the plugin.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     @get:Nested
     public abstract val core: DefaultCoreConfiguration
+
+    /**
+     * The publishing configuration of the plugin.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     @get:Nested
     public abstract val publish: DefaultPublishConfiguration
+
+    /**
+     * The build constants configuration of the plugin.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     @get:Nested
     public abstract val buildConstants: DefaultBuildConstantsConfiguration
+
+    /**
+     * The cinterop configuration of the plugin.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     @get:Nested
     public abstract val cinterop: DefaultCinteropConfiguration
+
+    /**
+     * The testing configuration of the plugin.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     @get:Nested
     public abstract val testing: DefaultTestingConfiguration
+
+    /**
+     * The docs configuration of the plugin.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     @get:Nested
     public abstract val docs: DefaultDocsConfiguration
 
+    /**
+     * The core configuration of the plugin.
+     *
+     * @param action The changes, that would be applied.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public fun core(action: Action<DefaultCoreConfiguration>) {
         action.execute(core)
     }
 
+    /**
+     * The publishing configuration of the plugin.
+     *
+     * @param action The changes, that would be applied.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public fun publish(action: Action<DefaultPublishConfiguration>) {
         action.execute(publish)
     }
 
+    /**
+     * The build constants configuration of the plugin.
+     *
+     * @param action The changes, that would be applied.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public fun buildConstants(action: Action<DefaultBuildConstantsConfiguration>) {
         action.execute(buildConstants)
     }
 
+    /**
+     * The cinterop configuration of the plugin.
+     *
+     * @param action The changes, that would be applied.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public fun cinterop(action: Action<DefaultCinteropConfiguration>) {
         action.execute(cinterop)
     }
 
+    /**
+     * The testing configuration of the plugin.
+     *
+     * @param action The changes, that would be applied.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public fun testing(action: Action<DefaultTestingConfiguration>) {
         action.execute(testing)
     }
 
+    /**
+     * The docs configuration of the plugin.
+     *
+     * @param action The changes, that would be applied.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
     public fun docs(action: Action<DefaultDocsConfiguration>) {
         action.execute(docs)
     }
