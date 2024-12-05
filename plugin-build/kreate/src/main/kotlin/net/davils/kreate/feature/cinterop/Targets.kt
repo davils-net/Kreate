cb9593cd747b53f66ebe8e6b76a159c3f196f8cb
Nils Jäkel <nils.jaekel@davils.net>
2024-12-05 14:43:26 +0100

    feat: added rudimentary cinterop support

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/cinterop/Targets.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/cinterop/Targets.kt
@@ -9,36 +9,71 @@ package net.davils.kreate.feature.cinterop
 
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithHostTests
 import org.jetbrains.kotlin.konan.target.KonanTarget
+import java.nio.file.Path
 
 public enum class Target(public val target: MutableList<KonanTarget>) {
     WINDOWS(mutableListOf(KonanTarget.MINGW_X64)),
     LINUX(mutableListOf(KonanTarget.LINUX_X64)),
     MACOS(mutableListOf(KonanTarget.MACOS_X64, KonanTarget.MACOS_ARM64)),
     IOS(mutableListOf(KonanTarget.IOS_ARM64, KonanTarget.IOS_X64, KonanTarget.IOS_SIMULATOR_ARM64)),
-    WATCHOS(mutableListOf(KonanTarget.WATCHOS_ARM32, KonanTarget.WATCHOS_ARM64, KonanTarget.WATCHOS_X64, KonanTarget.WATCHOS_SIMULATOR_ARM64)),
+    WATCHOS(
+        mutableListOf(
+            KonanTarget.WATCHOS_ARM32,
+            KonanTarget.WATCHOS_ARM64,
+            KonanTarget.WATCHOS_X64,
+            KonanTarget.WATCHOS_SIMULATOR_ARM64
+        )
+    ),
     TVOS(mutableListOf(KonanTarget.TVOS_ARM64, KonanTarget.TVOS_X64, KonanTarget.TVOS_SIMULATOR_ARM64));
 }
 
+internal fun KotlinNativeTargetWithHostTests.applyCInterop(path: Path, group: String) {
+    compilations.getByName("main") {
+        cinterops {
+            create("native") {
+                packageName = "$group.cinterop"
+                defFile(path)
+            }
+        }
+    }
+}
+
+internal fun KotlinNativeTarget.applyCInterop(path: Path, group: String) {
+    compilations.getByName("main") {
+        cinterops {
+            create("native") {
+                packageName = "$group.cinterop"
+                defFile(path)
+            }
+        }
+    }
+}
+
 public fun applyNativeTargets(project: Project, targets: List<Target>) {
+    val path = project.rootProject.file("cinterop").resolve("cinterop.def").toPath()
+    val group = project.group.toString()
+    
     project.extensions.configure<KotlinMultiplatformExtension>("kotlin") {
         targets.forEach { target ->
             target.target.forEach { nativeTarget ->
                 when (nativeTarget) {
-                    KonanTarget.MINGW_X64 -> mingwX64()
-                    KonanTarget.LINUX_X64 -> linuxX64()
-                    KonanTarget.MACOS_X64 -> macosX64()
-                    KonanTarget.MACOS_ARM64 -> macosArm64()
-                    KonanTarget.IOS_ARM64 -> iosArm64()
-                    KonanTarget.IOS_X64 -> iosX64()
-                    KonanTarget.IOS_SIMULATOR_ARM64 -> iosSimulatorArm64()
-                    KonanTarget.WATCHOS_ARM32 -> watchosArm32()
-                    KonanTarget.WATCHOS_ARM64 -> watchosArm64()
-                    KonanTarget.WATCHOS_X64 -> watchosX64()
-                    KonanTarget.WATCHOS_SIMULATOR_ARM64 -> watchosSimulatorArm64()
-                    KonanTarget.TVOS_ARM64 -> tvosArm64()
-                    KonanTarget.TVOS_X64 -> tvosX64()
-                    KonanTarget.TVOS_SIMULATOR_ARM64 -> tvosSimulatorArm64()
+                    KonanTarget.MINGW_X64 -> mingwX64 { applyCInterop(path, group) }
+                    KonanTarget.LINUX_X64 -> linuxX64 { applyCInterop(path, group) }
+                    KonanTarget.MACOS_X64 -> macosX64 { applyCInterop(path, group) }
+                    KonanTarget.MACOS_ARM64 -> macosArm64 { applyCInterop(path, group) }
+                    KonanTarget.IOS_ARM64 -> iosArm64 { applyCInterop(path, group) }
+                    KonanTarget.IOS_X64 -> iosX64 { applyCInterop(path, group) }
+                    KonanTarget.IOS_SIMULATOR_ARM64 -> iosSimulatorArm64 { applyCInterop(path, group) }
+                    KonanTarget.WATCHOS_ARM32 -> watchosArm32 { applyCInterop(path, group) }
+                    KonanTarget.WATCHOS_ARM64 -> watchosArm64 { applyCInterop(path, group) }
+                    KonanTarget.WATCHOS_X64 -> watchosX64 { applyCInterop(path, group) }
+                    KonanTarget.WATCHOS_SIMULATOR_ARM64 -> watchosSimulatorArm64 { applyCInterop(path, group) }
+                    KonanTarget.TVOS_ARM64 -> tvosArm64 { applyCInterop(path, group) }
+                    KonanTarget.TVOS_X64 -> tvosX64 { applyCInterop(path, group) }
+                    KonanTarget.TVOS_SIMULATOR_ARM64 -> tvosSimulatorArm64 { applyCInterop(path, group) }
                     else -> throw IllegalStateException("Unknown target: $target")
                 }
             }
