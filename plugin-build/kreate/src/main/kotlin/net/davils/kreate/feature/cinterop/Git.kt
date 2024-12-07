f6974e3e6cc19f94d4c5e720cce705b54170592a
Nils Jäkel <nils.jaekel@davils.net>
2024-12-07 17:11:03 +0100

    feat: added task to exclude some compiler output from rust project

---
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/cinterop/Git.kt
@@ -0,0 +1,52 @@
+/*
+ * All rights reserved
+ * Copyright 2024 Davils
+ *
+ * Unauthorized copying, distribution, or modification of this work is strictly prohibited.
+ */
+
+package net.davils.kreate.feature.cinterop
+
+import net.davils.kreate.feature.Task
+import net.davils.kreate.Paths
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * Represents the Task to exclude some compiler artifacts from git.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
+public abstract class ExcludeSourcesInGit : Task() {
+    /**
+     * The path handler.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    private val paths = Paths(project)
+
+    /**
+     * The task action. It excludes some compiler artifacts from git.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    @TaskAction
+    override fun execute() {
+        if (!paths.gitignore.exists()) {
+            paths.gitignore.createNewFile()
+        }
+
+        val gitignore = paths.gitignore.readText()
+        if (!gitignore.contains("### Rust ###")) {
+            paths.gitignore.appendText("\n\n### Rust ###")
+        }
+        if (!gitignore.contains("target")) {
+            paths.gitignore.appendText("\ntarget")
+        }
+        if (!gitignore.contains("cinterop")) {
+            paths.gitignore.appendText("\ncinterop")
+        }
+    }
+}
