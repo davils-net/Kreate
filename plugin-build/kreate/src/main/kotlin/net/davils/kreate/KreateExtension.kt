0582090dcf61f819375b3698ff6264d99d18282d
Nils Jäkel <nils.jaekel@davils.net>
2024-12-07 17:10:21 +0100

    refactor: used only one configuration

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/KreateExtension.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/KreateExtension.kt
@@ -7,12 +7,12 @@
 
 package net.davils.kreate
 
-import net.davils.kreate.feature.buildconstants.DefaultBuildConstantsConfiguration
-import net.davils.kreate.feature.cinterop.DefaultCInteropConfiguration
-import net.davils.kreate.feature.core.DefaultCoreConfiguration
-import net.davils.kreate.feature.docs.DefaultDocsConfiguration
-import net.davils.kreate.feature.publish.DefaultPublishConfiguration
-import net.davils.kreate.feature.testing.DefaultTestingConfiguration
+import net.davils.kreate.feature.buildconstants.BuildConstantsConfiguration
+import net.davils.kreate.feature.cinterop.CInteropConfiguration
+import net.davils.kreate.feature.core.CoreConfiguration
+import net.davils.kreate.feature.docs.DocsConfiguration
+import net.davils.kreate.feature.publish.PublishConfiguration
+import net.davils.kreate.feature.testing.TestingConfiguration
 import org.gradle.api.Action
 import org.gradle.api.tasks.Nested
 
@@ -24,128 +24,128 @@ import org.gradle.api.tasks.Nested
  * */
 public abstract class KreateExtension {
     /**
-     * The core configuration of the plugin.
+     * The default core configuration of the plugin.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
     @get:Nested
-    public abstract val core: DefaultCoreConfiguration
+    public abstract val core: CoreConfiguration
 
     /**
-     * The publishing configuration of the plugin.
+     * The default publishing configuration of the plugin.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
     @get:Nested
-    public abstract val publish: DefaultPublishConfiguration
+    public abstract val publish: PublishConfiguration
 
     /**
-     * The build constants configuration of the plugin.
+     * The default build constants configuration of the plugin.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
     @get:Nested
-    public abstract val buildConstants: DefaultBuildConstantsConfiguration
+    public abstract val buildConstants: BuildConstantsConfiguration
 
     /**
-     * The cinterop configuration of the plugin.
+     * The default cinterop configuration of the plugin.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
     @get:Nested
-    public abstract val cinterop: DefaultCInteropConfiguration
+    public abstract val cinterop: CInteropConfiguration
 
     /**
-     * The testing configuration of the plugin.
+     * The default testing configuration of the plugin.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
     @get:Nested
-    public abstract val testing: DefaultTestingConfiguration
+    public abstract val testing: TestingConfiguration
 
     /**
-     * The docs configuration of the plugin.
+     * The default docs configuration of the plugin.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
     @get:Nested
-    public abstract val docs: DefaultDocsConfiguration
+    public abstract val docs: DocsConfiguration
 
     /**
-     * The core configuration of the plugin.
+     * The default core configuration of the plugin.
      *
-     * @param action The changes, that would be applied.
+     * @param action The changes, that should be applied.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    public fun core(action: Action<DefaultCoreConfiguration>) {
+    public fun core(action: Action<CoreConfiguration>) {
         action.execute(core)
     }
 
     /**
-     * The publishing configuration of the plugin.
+     * The default publishing configuration of the plugin.
      *
-     * @param action The changes, that would be applied.
+     * @param action The changes, that should be applied.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    public fun publish(action: Action<DefaultPublishConfiguration>) {
+    public fun publish(action: Action<PublishConfiguration>) {
         action.execute(publish)
     }
 
     /**
-     * The build constants configuration of the plugin.
+     * The default build constants configuration of the plugin.
      *
-     * @param action The changes, that would be applied.
+     * @param action The changes, that should be applied.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    public fun buildConstants(action: Action<DefaultBuildConstantsConfiguration>) {
+    public fun buildConstants(action: Action<BuildConstantsConfiguration>) {
         action.execute(buildConstants)
     }
 
     /**
-     * The cinterop configuration of the plugin.
+     * The default cinterop configuration of the plugin.
      *
-     * @param action The changes, that would be applied.
+     * @param action The changes, that should be applied.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    public fun cinterop(action: Action<DefaultCInteropConfiguration>) {
+    public fun cinterop(action: Action<CInteropConfiguration>) {
         action.execute(cinterop)
     }
 
     /**
-     * The testing configuration of the plugin.
+     * The default testing configuration of the plugin.
      *
-     * @param action The changes, that would be applied.
+     * @param action The changes, that should be applied.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    public fun testing(action: Action<DefaultTestingConfiguration>) {
+    public fun testing(action: Action<TestingConfiguration>) {
         action.execute(testing)
     }
 
     /**
-     * The docs configuration of the plugin.
+     * The default docs configuration of the plugin.
      *
-     * @param action The changes, that would be applied.
+     * @param action The changes, that should be applied.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    public fun docs(action: Action<DefaultDocsConfiguration>) {
+    public fun docs(action: Action<DocsConfiguration>) {
         action.execute(docs)
     }
 }
