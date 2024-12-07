305b7f235f978ebe2fc682f698b0df91789cd0f4
Nils Jäkel <nils.jaekel@davils.net>
2024-12-07 17:07:43 +0100

    docs: added documentation

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/Task.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/Task.kt
@@ -9,34 +9,33 @@ package net.davils.kreate.feature
 
 import net.davils.kreate.KreateExtension
 import net.davils.kreate.build.BuildConstants
+import net.davils.kreate.kreateExtension
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 /**
- * Represents a kreate task.
+ * Central adapter for all tasks in kreate.
  *
  * @since 0.0.1
  * @author Nils Jäkel
  * */
 public abstract class Task : DefaultTask() {
     /**
-     * The kreate extension.
+     * The kreate extension from the current gradle project.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
     @get:Input
-    internal val extension: KreateExtension = project.extensions.getByType<KreateExtension>()
+    internal val extension: KreateExtension = project.kreateExtension
 
     /**
-     * The task to execute.
+     * The task, that should be executed if the task is triggered.
      *
      * @since 0.0.1
      * @author Nils Jäkel
@@ -44,6 +43,14 @@ public abstract class Task : DefaultTask() {
     public abstract fun execute()
 }
 
+/**
+ * Executes a task before any compilation.
+ *
+ * @param task The task, that should be executed before compilation.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
 internal fun Project.execTaskBeforeCompile(task: Task) {
     tasks.withType<KotlinCompilationTask<*>> {
         dependsOn(task)
@@ -54,10 +61,23 @@ internal fun Project.execTaskBeforeCompile(task: Task) {
     }
 }
 
+/**
+ * Registers a task in the current gradle project under the group [BuildConstants.ORGANIZATION_NAME].
+ *
+ * @param name The name of the task.
+ * @param description The description of the task.
+ * @param inject Additional configuration for the task.
+ * @param T The type of the task.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ *
+ * @see Task
+ * */
 internal inline fun <reified T : Task> Project.registerTask(
     name: String,
     description: String,
-    noinline inject: T.() -> Unit = {},
+    crossinline inject: T.() -> Unit = {},
 ): TaskProvider<T?> {
     return tasks.register(name, T::class.java) {
         this.group = BuildConstants.ORGANIZATION_NAME.lowercase()
