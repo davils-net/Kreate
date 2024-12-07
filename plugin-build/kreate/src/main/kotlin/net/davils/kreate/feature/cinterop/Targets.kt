65f37e88be39baf0a8eccf7a05a9ff2c9fb815ca
Nils Jäkel <nils.jaekel@davils.net>
2024-12-07 17:08:17 +0100

    docs: added documentation

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/cinterop/Targets.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/cinterop/Targets.kt
@@ -7,79 +7,112 @@
 
 package net.davils.kreate.feature.cinterop
 
-import net.davils.kreate.feature.Task
-import org.gradle.api.tasks.TaskAction
+import net.davils.kreate.KreateExtension
+import net.davils.kreate.Paths
+import net.davils.kreate.build.BuildConstants
+import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithHostTests
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import java.nio.file.Path
 
-public abstract class ApplyNativeTargets : Task() {
-    private val projectTargets = extension.cinterop.targets.get()
-    private val path = project.rootProject.file("cinterop").resolve("cinterop.def").toPath()
-    private val group = project.group.toString()
+/**
+ * Applies the native targets to the gradle project.
+ *
+ * @param project The gradle current gradle project.
+ * @param extension The kreate extension.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
+internal fun applyNativeTargets(project: Project, extension: KreateExtension) {
+    val paths = Paths(project)
+    val projectTargets = extension.cinterop.targets.get()
+    val cinteropFile = paths.cinteropFile.toPath()
 
-    @TaskAction
-    override fun execute() {
-        project.extensions.configure<KotlinMultiplatformExtension>("kotlin") {
+    project.extensions.configure<KotlinMultiplatformExtension>("kotlin") {
         projectTargets.forEach { target ->
             target.target.forEach { nativeTarget ->
                 when (nativeTarget) {
-                    KonanTarget.MINGW_X64 -> mingwX64 { applyCInterop(path, group) }
-                    KonanTarget.LINUX_X64 -> linuxX64 { applyCInterop(path, group) }
-                    KonanTarget.MACOS_X64 -> macosX64 { applyCInterop(path, group) }
-                    KonanTarget.MACOS_ARM64 -> macosArm64 { applyCInterop(path, group) }
-                    KonanTarget.IOS_ARM64 -> iosArm64 { applyCInterop(path, group) }
-                    KonanTarget.IOS_X64 -> iosX64 { applyCInterop(path, group) }
-                    KonanTarget.IOS_SIMULATOR_ARM64 -> iosSimulatorArm64 { applyCInterop(path, group) }
-                    KonanTarget.WATCHOS_ARM32 -> watchosArm32 { applyCInterop(path, group) }
-                    KonanTarget.WATCHOS_ARM64 -> watchosArm64 { applyCInterop(path, group) }
-                    KonanTarget.WATCHOS_X64 -> watchosX64 { applyCInterop(path, group) }
-                    KonanTarget.WATCHOS_SIMULATOR_ARM64 -> watchosSimulatorArm64 { applyCInterop(path, group) }
-                    KonanTarget.TVOS_ARM64 -> tvosArm64 { applyCInterop(path, group) }
-                    KonanTarget.TVOS_X64 -> tvosX64 { applyCInterop(path, group) }
-                    KonanTarget.TVOS_SIMULATOR_ARM64 -> tvosSimulatorArm64 { applyCInterop(path, group) }
+                    KonanTarget.MINGW_X64 -> mingwX64 { applyCInterop(cinteropFile) }
+                    KonanTarget.LINUX_X64 -> linuxX64 { applyCInterop(cinteropFile) }
+                    KonanTarget.MACOS_X64 -> macosX64 { applyCInterop(cinteropFile) }
+                    KonanTarget.MACOS_ARM64 -> macosArm64 { applyCInterop(cinteropFile) }
+                    KonanTarget.IOS_ARM64 -> iosArm64 { applyCInterop(cinteropFile) }
+                    KonanTarget.IOS_X64 -> iosX64 { applyCInterop(cinteropFile) }
+                    KonanTarget.IOS_SIMULATOR_ARM64 -> iosSimulatorArm64 { applyCInterop(cinteropFile) }
+                    KonanTarget.WATCHOS_ARM32 -> watchosArm32 { applyCInterop(cinteropFile) }
+                    KonanTarget.WATCHOS_ARM64 -> watchosArm64 { applyCInterop(cinteropFile) }
+                    KonanTarget.WATCHOS_X64 -> watchosX64 { applyCInterop(cinteropFile) }
+                    KonanTarget.WATCHOS_SIMULATOR_ARM64 -> watchosSimulatorArm64 { applyCInterop(cinteropFile) }
+                    KonanTarget.TVOS_ARM64 -> tvosArm64 { applyCInterop(cinteropFile) }
+                    KonanTarget.TVOS_X64 -> tvosX64 { applyCInterop(cinteropFile) }
+                    KonanTarget.TVOS_SIMULATOR_ARM64 -> tvosSimulatorArm64 { applyCInterop(cinteropFile) }
                     else -> throw IllegalStateException("Unknown target: $target")
                 }
             }
         }
     }
-    }
 }
 
-public enum class Target(public val target: MutableList<KonanTarget>) {
+/**
+ * The available targets for the native cinterop.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
+public enum class Target(
+    /**
+     * A [List] of [KonanTarget] that should be applied to the target.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     *
+     * @see KonanTarget
+     * */
+    public val target: MutableList<KonanTarget>
+) {
     WINDOWS(mutableListOf(KonanTarget.MINGW_X64)),
     LINUX(mutableListOf(KonanTarget.LINUX_X64)),
     MACOS(mutableListOf(KonanTarget.MACOS_X64, KonanTarget.MACOS_ARM64)),
     IOS(mutableListOf(KonanTarget.IOS_ARM64, KonanTarget.IOS_X64, KonanTarget.IOS_SIMULATOR_ARM64)),
-    WATCHOS(
-        mutableListOf(
-            KonanTarget.WATCHOS_ARM32,
-            KonanTarget.WATCHOS_ARM64,
-            KonanTarget.WATCHOS_X64,
-            KonanTarget.WATCHOS_SIMULATOR_ARM64
-        )
-    ),
+    WATCHOS(mutableListOf(KonanTarget.WATCHOS_ARM32, KonanTarget.WATCHOS_ARM64, KonanTarget.WATCHOS_X64, KonanTarget.WATCHOS_SIMULATOR_ARM64)),
     TVOS(mutableListOf(KonanTarget.TVOS_ARM64, KonanTarget.TVOS_X64, KonanTarget.TVOS_SIMULATOR_ARM64));
 }
 
-internal fun KotlinNativeTargetWithHostTests.applyCInterop(path: Path, group: String) {
+/**
+ * Applies the cinterop to the specific native target.
+ *
+ * @param path The path to the cinterop file.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
+internal fun KotlinNativeTargetWithHostTests.applyCInterop(path: Path) {
     compilations.getByName("main") {
         cinterops {
             create("native") {
-                packageName = "$group.cinterop"
+                packageName = "${BuildConstants.GROUP}.cinterop"
                 defFile(path)
             }
         }
     }
 }
 
-internal fun KotlinNativeTarget.applyCInterop(path: Path, group: String) {
+/**
+ * Applies the cinterop to the specific native target.
+ *
+ * @param path The path to the cinterop file.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ * */
+internal fun KotlinNativeTarget.applyCInterop(path: Path) {
     compilations.getByName("main") {
         cinterops {
             create("native") {
-                packageName = "$group.cinterop"
+                packageName = "${BuildConstants.GROUP}.cinterop"
                 defFile(path)
             }
         }
