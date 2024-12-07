07be5ca64bdd5bb980f4cb0169697f38853b2c13
Nils Jäkel <nils.jaekel@davils.net>
2024-12-07 17:07:13 +0100

    refactor: using on configuration for testing

--- plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/testing/TestingConfiguration.kt
+++ plugin-build/kreate/src/main/kotlin/net/davils/kreate/feature/testing/TestingConfiguration.kt
@@ -7,34 +7,25 @@
 
 package net.davils.kreate.feature.testing
 
-import net.davils.kreate.utils.KreateFeatureConfiguration
+import net.davils.kreate.feature.KreateFeatureConfiguration
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import javax.inject.Inject
 
 /**
- * Creates the configuration for the testing feature.
+ * Represents the configuration for the testing feature.
  *
  * @since 0.0.1
  * @author Nils Jäkel
  * */
-internal interface TestingConfiguration : KreateFeatureConfiguration {
+public abstract class TestingConfiguration @Inject constructor(objects: ObjectFactory) : KreateFeatureConfiguration {
+    override val enabled: Property<Boolean> = objects.property(Boolean::class.java).apply { set(true) }
+
     /**
-     * Indicates if a test report should be created.
+     * If a test report should be created.
      *
      * @since 0.0.1
      * @author Nils Jäkel
      * */
-    val createTestReport: Property<Boolean>
-}
-
-/**
- * Default configuration for the testing feature.
- *
- * @since 0.0.1
- * @author Nils Jäkel
- * */
-public abstract class DefaultTestingConfiguration @Inject constructor(objects: ObjectFactory) : TestingConfiguration {
-    override val enabled: Property<Boolean> = objects.property(Boolean::class.java).apply { set(true) }
-    override val createTestReport: Property<Boolean> = objects.property(Boolean::class.java).apply { set(true) }
+    public val createTestReport: Property<Boolean> = objects.property(Boolean::class.java).apply { set(true) }
 }
